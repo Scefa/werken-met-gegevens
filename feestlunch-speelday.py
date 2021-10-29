037545e8019a4272f30f8964dd27bb3e3ae0571b
Scefa <89840922+Scefa@users.noreply.github.com>
2021-10-29 10:07:05 +0200

    Added Feestlunch and Speelhaldag

---
+++ feestlunch-speelday.py
@@ -0,0 +1,13 @@
+Croissant = 17
+CroissantPrijs = 0.39
+Stokbroden = 2
+StokbrodenPrijs = 2.78
+Kortingbonnen = 1.5
+print(Croissant * CroissantPrijs + (Stokbroden * StokbrodenPrijs) - Kortingbonnen)
+
+Toegangsticket = 7.45
+Personen = 4
+VRGameSeatPrijs = 0.37
+Tijdx5Min = 9
+
+print(Toegangsticket * Personen + (VRGameSeatPrijs * Tijdx5Min))
