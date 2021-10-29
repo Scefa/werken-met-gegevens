35180177b9989d85b4a42c3d6881b4999be9369c
Scefa <89840922+Scefa@users.noreply.github.com>
2021-10-29 13:47:48 +0200

    Create factuurbericht.py

---
+++ factuurbericht.py
@@ -0,0 +1,9 @@
+Bedrag = 10.69
+Factuurtekst = 'De feestlunch kost je bij de bakker ' + str(Bedrag) + 'euro' ' voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!'
+
+print(Factuurtekst)
+
+Prijs = 44.10
+Bericht = 'Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar ' +str(Prijs) + ' Met de korting'
+
+print(Bericht)
